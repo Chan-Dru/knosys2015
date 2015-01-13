39a798795575f3d4217973a4c2c7443407ff3d47
unknown <chandrunitpy@gmail.com>
2015-01-13 21:26:04 +0530

    'button'

--- change/login.php
+++ change/login.php
@@ -4,7 +4,7 @@ try{
  $username = $_POST['name'];
  $password = ($_POST['pwd']);
 
-/*define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
+define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
 define('DB_PORT',getenv('OPENSHIFT_MYSQL_DB_PORT')); 
 define('DB_USER',getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
 define('DB_PASS',getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
@@ -12,22 +12,23 @@ define('DB_NAME',getenv('OPENSHIFT_GEAR_NAME'));
 
 $dsn = 'mysql:dbname=login;host='.DB_HOST.';port='.DB_PORT;
 $db = new PDO($dsn, DB_USER, DB_PASS);
-*/
+
 
 
 
  
- $db=new PDO("mysql:host=localhost;dbname=login",'root','');
+ /*$db=new PDO("mysql:host=localhost;dbname=login",'root','');*/
  $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_WARNING);
  $sql = "SELECT * FROM user WHERE username='$username' AND password='$password'";
  $result =$db->query($sql);
- $result3=$result->fetch();
-
- 
- if( $result3!=null) {
+ $result1=$result->fetch();
+ $num_row = count($result1);
+ $user=$result->fetchColumn(1);
+ $id=$result->fetchColumn(0);
+ if( $num_row >=1 ) {
   echo 'true';
-  $_SESSION['user_name']=$username;
-  $_SESSION['user_id']=$result3['id'];
+  $_SESSION['user_name']=$user;
+  $_SESSION['user_id']=$id;
  }
  else{
  echo 'false';
--- change/signupindex.php
+++ change/signupindex.php
@@ -25,8 +25,9 @@ $(document).ready(function(){
     {
      $("#login_form").html('<div class="alert alert-success" >You have  successfully logged in!</div>');
      
+    /* $("#profile").html("<a href='logout.php' id='logout'>Logout</a>");*/
 
-     $('#after_login').html("Hi,"+username+"<a href='logout.php' id='logout'><img class='menu-button col-lg-1' src='pins/logout.png'/></a>");
+     $('#after_login').html('<a href="logout.php" id="logout"><img class="menu-button col-lg-1" src="pins/logout.png"/></a>');
 
     }
     else
@@ -71,19 +72,16 @@ $(document).ready(function(){
 }
 /*.col-lg-1{
   width:9%;
-}*/
+}
 .col-lg-2{
   width:14.5%;
-}
+}*/
 .modal .modal-body{
   max-height:480px;
 }
 .navbar{
   background-color: rgba(0,0,0,0);
   border:0px;
-}
-#after_login{
-  color:white;
 }
 </style>
 
@@ -108,11 +106,9 @@ $(document).ready(function(){
 
 <div  id="profile" class="row">
   <div class="col-lg-2"></div>
-   <?php if(isset($_SESSION['user_id'])){
-  
-    echo "<div id='after_login'><a href='logout.php' id='logout'><img class='menu-button col-lg-1' src='pins/logout.png'/></a>Hi,".$_SESSION['user_name']."</div>";
+   <?php if(isset($_SESSION['user_name'])){
   ?>
-  
+  <a href="logout.php" id="logout"><img class="menu-button col-lg-1" src="pins/logout.png"/></a>
 
 <?php }else {?>
   <div id="after_login">
