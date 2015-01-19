6608e19d942e919f8e2f2bc4c1efefb39ea56f83
unknown <chandrunitpy@gmail.com>
2015-01-19 21:48:21 +0530

    'late'

--- home1.php
+++ home1.php
@@ -112,7 +112,7 @@ include('signupindex.php');
           <div id="codeChef" data-toggle="modal" data-target=".CutItShort"><img class="level5" src="svg/shortFlim.svg"/></div>
           <div id="treasurHunt" data-toggle="modal" data-target=".Cob_Web"><img class="level5" src="svg/maze.svg"/></div>
           <div id="photography" data-toggle="modal" data-target=".ShootTheFrame"><img class="level4" src="svg/shootTheFrame.svg"/></div>
-          <div id="webEvent" data-toggle="modal" data-target=".webProjectX"><img class="level4" src="svg/webprojectx.svg"/></div>
+          <div id="webEvent" data-toggle="modal" data-target=".Cob_Web"><img class="level4" src="svg/webprojectx.svg"/></div>
         </div>
         <div id="brain-sub" class="col-lg-11 col-md-11 col-sm-11 col-xs-11">
           <div id="PP" data-toggle="modal" data-target=".PP"><img class="level6" src="svg/ityuktha.svg"/></div>
